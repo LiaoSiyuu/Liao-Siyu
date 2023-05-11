ef3ffb962acf09fdf5822f40dbff745a44629161
Siyuu <liaosiyuu@gmail.com>
2023-05-11 16:29:04 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,19 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Do Androids Dream of Electric Sheep? ', 'Merry Christmas Mr. Lawrence'], // 首页大图标语文字
+  // HOME_BANNER_GREETINGS: ['Do Androids Dream of Electric Sheep? ', 'Merry Christmas Mr. Lawrence'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: [
+    'Because we don\'t know when we will die',
+    'we get to think of life as an inexhaustible well',
+    'And yet everything happens only a certain number of times',
+    'and a very small number really',
+    'How many more times will you remember a certain afternoon of your childhood',
+    'some afternoon that\'s so deeply a part of your being that you can\'t even conceive of your life without it',
+    'Perhaps four',
+    'or five times more',
+    'Perhaps not even that',
+    'How many more times will you watch the full moon rise',
+    'Perhaps twenty',
+    'And yet it all seems limitless'],
 
   HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
